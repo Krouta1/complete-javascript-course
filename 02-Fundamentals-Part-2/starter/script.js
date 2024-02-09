75be4f20ee7a913385b2c385a5386f915f39dbcd
Petr Kroutil <116441828+Krouta1@users.noreply.github.com>
2024-02-09 10:44:29 +0100

    Lesson 32

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -11,3 +11,16 @@ if (hasDriversLicense) console.log('I can drive :D');
 const interface = 'Audio'; // Uncaught SyntaxError: Unexpected strict mode reserved word, when in strict mode, without the strict mode, it will not throw an error
 
 //Lesson - 32: Functions
+// function logger (){
+//     console.log("Hello world.");
+// };
+// logger();
+
+// function fruitProcessor ( apples, oranges ){
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+//     return juice
+// }
+// console.log(fruitProcessor(6,4));
+
+// const appleOrangeJuice = fruitProcessor(4,6)
+// console.log(appleOrangeJuice);
