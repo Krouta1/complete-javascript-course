498c512291ebda7f8a88b41a5895be97603ab430
Petr Kroutil <pkroutil1@seznam.cz>
2024-02-11 15:41:28 +0100

    Lesson: 57 - 73

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+
+//Lesson 72: Selecting and Manipulating Elements
+
+// document.querySelector('.message').textContent = 'Correct Number! 🎉';
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+// document.querySelector('.guess').value = 23;
+
+//Lesson 73: Handling Click Events
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  // console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+  }
+});
