f134f51bbd056b796d21f9c57ec3ed80197b47e9
Petr Kroutil <pkroutil1@seznam.cz>
2024-02-19 05:18:45 +0100

    Lesson: 143 -  157

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -74,3 +74,260 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+// Lesson 143 - Simple Array Methods
+
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// // SLICE
+// console.log(arr.slice(2)); // ['c', 'd', 'e'], returns a new array
+// console.log(arr.slice(2, 4)); // ['c', 'd'], returns a new array
+// console.log(arr.slice(-2)); // ['d', 'e'], returns a new array
+// console.log(arr.slice(-1)); // ['e'], returns a new array
+// console.log(arr.slice(1, -2)); // ['b', 'c'], returns a new array
+// console.log(arr.slice()); // ['a', 'b', 'c', 'd', 'e'], returns a new array, shallow copy
+
+// // SPLICE
+// console.log(arr.splice(2)); // ['c', 'd', 'e'], mutates the original array
+// console.log(arr); // ['a', 'b'], original array is now mutated
+// arr = ['a', 'b', 'c', 'd', 'e']; // reset the array
+// console.log(arr.splice(-1)); // ['e'], mutates the original array
+
+// // REVERSE
+// arr = ['a', 'b', 'c', 'd', 'e']; // reset the array
+// let arr2 = ['j', 'i', 'h', 'g', 'f'];
+// console.log(arr2.reverse()); // ['f', 'g', 'h', 'i', 'j'], mutates the original array
+
+// // CONCAT
+// const letters = arr.concat(arr2); // ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'], returns a new array
+
+// // JOIN
+// console.log(letters.join(' - ')); // a - b - c - d - e - f - g - h - i - j
+
+// Lesson 144 -  new at array method
+
+// const arr = [1, 2, 3, 4, 5, 6, 7];
+// console.log(arr[0]); // 1
+// console.log(arr.at(0)); // 1 same as above
+
+// console.log(arr[arr.length - 1]); // 7
+// console.log(arr.slice(-1)[0]); // [7], returns a new array
+// console.log(arr.at(-1)); // 7 same as above
+// console.log('petr'.at(-1)); // r
+
+// // Lesson 145 - forEach loop
+
+// for (const [i, movement] of movements.entries()) {
+//   if (movement > 0) {
+//     console.log(`Movement ${i + 1}: You deposited ${movement}`);
+//   } else {
+//     console.log(`You withdrew ${Math.abs(movement)}`);
+//   }
+// }
+
+// // forEach loop
+// console.log('---');
+// movements.forEach(function (movement, i, arr) {
+//   if (movement > 0) {
+//     console.log(`Movement ${i + 1}: You deposited ${movement}`);
+//     console.log(arr);
+//   } else {
+//     console.log(`You withdrew ${Math.abs(movement)}`);
+//   }
+// });
+
+// Lesson 146 - forEach with maps and sets
+
+// Map
+
+// currencies.forEach(function (value, key, map) {
+//   console.log(`${key}: ${value}`);
+// });
+
+// // Set
+// const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+// console.log(currenciesUnique); // Set(3) {"USD", "GBP", "EUR"}
+// currenciesUnique.forEach(function (value, _, set) {
+//   console.log(`${value}: ${value}`);
+// });
+
+// Lesson 148 - Creating DOM elements
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+        <div class="movements__value">${mov}€</div>
+      </div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+// Lesson 149 - Coding challenge #1
+
+// const dogsJulia = [3, 5, 2, 12, 7];
+// const dogsKate = [4, 1, 15, 8, 3];
+
+// const checkDogs = function (dogsJulia, dogsKate) {
+//   const dogsJuliaCorrected = dogsJulia.slice(1, -2);
+//   const dogs = dogsJuliaCorrected.concat(dogsKate);
+//   dogs.forEach(function (dog, i) {
+//     if (dog >= 3) {
+//       console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`);
+//     } else {
+//       console.log(`Dog number ${i + 1} is still a puppy`);
+//     }
+//   });
+// };
+
+// checkDogs(dogsJulia, dogsKate);
+
+// Lesson 151 - The map method
+
+// const eurToUsd = 1.1;
+
+// const movementsUSD = movements.map(mov => {
+//   return mov * eurToUsd;
+// });
+
+// console.log(movements);
+// console.log(movementsUSD);
+
+// const movementDescriptions = movements.map((mov, i, arr) => {
+//   return `Movement ${i + 1}: You ${
+//     mov > 0 ? 'deposited' : 'withdrew'
+//   } ${Math.abs(mov)}`;
+// });
+
+// console.log(...movementDescriptions);
+
+// Lesson 152 - Computing usernames
+
+const createUsernames = function (accounts) {
+  accounts.forEach(function (account) {
+    account.username = account.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+createUsernames(accounts);
+// console.log(accounts);
+
+// Lesson 153 - The filter method
+
+// const deposits = movements.filter(mov => mov > 0);
+// const withdrawals = movements.filter(mov => mov < 0);
+
+// console.log(deposits);
+// console.log(withdrawals);
+
+// Lesson 154 - The reduce method
+
+// it's like a snowball effect, it accumulates the value of the array
+// const balance = movements.reduce((acc, mov) => acc + mov, 0);
+// console.log(balance);
+
+const calcAndDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance}€`;
+};
+
+// //Maximum value of the array
+// const max = movements.reduce((acc, mov) => {
+//   if (acc > mov) {
+//     return acc;
+//   } else {
+//     return mov;
+//   }
+// }, movements[0]);
+// console.log(max);
+
+//Coding challenge #2
+
+// const calcAverageHumanAge = function (ages) {
+//   const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+//   const adults = humanAges.filter(age => age >= 18);
+//   const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+//   return average;
+// };
+
+// const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+// const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+// console.log(avg1, avg2);
+
+// Lesson 155 - The magic of chaining methods
+
+// const eurToUsd = 1.1;
+// const totalDeposistsUSD = movements
+//   .filter(mov => mov > 0)
+//   .map(mov => mov * eurToUsd)
+//   .reduce((acc, mov) => acc + mov, 0);
+// console.log(totalDeposistsUSD);
+
+const calcDisplaySummary = function (acc) {
+  const deposits = acc.movements.filter(mov => mov > 0);
+  const withdrawals = acc.movements.filter(mov => mov < 0);
+  const interest = acc.movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * acc.interestRate) / 100)
+    .filter((int, i, arr) => {
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumIn.textContent = `${deposits.reduce((acc, mov) => acc + mov, 0)}€`;
+  labelSumOut.textContent = `${Math.abs(
+    withdrawals.reduce((acc, mov) => acc + mov, 0)
+  )}€`;
+  labelSumInterest.textContent = `${interest}€`;
+};
+
+// Lesson 157 - Coding challenge #3
+
+// const calcAverageHumanAge = function (ages) {
+//   const average = ages
+//     .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+//     .filter(age => age >= 18)
+//     .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+//   return average;
+// };
+
+// const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+// const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+// console.log(avg1, avg2);
+
+// Lesson 158 - The find method
+
+// const firstWithdrawal = movements.find(mov => mov < 0);
+// console.log(firstWithdrawal);
+
+// const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+
+// Lesson 159 - Login funcitonality
+let currentAccount;
+
+//event handleres
+btnLogin.addEventListener('click', function (e) {
+  e.preventDefault();
+  const currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(' ')[0]
+    }`;
+    containerApp.style.opacity = 100;
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
+    displayMovements(currentAccount.movements);
+    calcAndDisplayBalance(currentAccount.movements);
+    calcDisplaySummary(currentAccount);
+  }
+});
