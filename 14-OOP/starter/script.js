b6bf695219240f47a0a681eec869ed2445388d9d
Petr Kroutil <pkroutil1@seznam.cz>
2024-02-27 20:32:09 +0100

    Lesson: 218 - 224

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -132,6 +132,139 @@
 
 //Lesson 217: Object.create
 
+// const PersonProto = {
+//   calcAge() {
+//     console.log(2037 - this.birthYear);
+//   },
+//   init(firstName, birthYear) {
+//     this.firstName = firstName;
+//     this.birthYear = birthYear;
+//   },
+// };
+
+// const steven = Object.create(PersonProto);
+// steven.init('Steven', 1991);
+// console.log(steven);
+
+// //Lesson 218: Coding challenge #2
+
+// class CarCl {
+//   constructor(make, speed) {
+//     this.make = make;
+//     this.speed = speed;
+//   }
+//   accelerate() {
+//     console.log(`${this.make} gained speed: ${this.speed + 10}km/h`);
+//   }
+
+//   brake() {
+//     console.log(`${this.make} lost speed: ${this.speed - 5}km/h`);
+//   }
+//   get speedUS() {
+//     return this.speed / 1.6;
+//   }
+//   set speedUS(speed) {
+//     this.speed = speed * 1.6;
+//   }
+// }
+
+// const ford = new CarCl('Ford', 120);
+// console.log(ford.speedUS);
+// ford.speedUS = 50;
+// console.log(ford);
+
+//Lesson 219: Inheritance between "Classes": Constructor Functions
+
+// const Person = function (firstName, birthYear) {
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
+// };
+
+// Person.prototype.calcAge = function () {
+//   console.log(2037 - this.birthYear);
+// };
+
+// const Student = function (firstName, birthYear, course) {
+//   Person.call(this, firstName, birthYear);
+//   this.course = course;
+// };
+
+// //Linking prototypes
+// Student.prototype = Object.create(Person.prototype);
+
+// Student.prototype.introduce = function () {
+//   console.log(`My name is ${this.firstName} and I study ${this.course}`);
+// };
+
+// Student.prototype.constructor = Student;
+
+// const mike = new Student('Mike', 2020, 'Computer Science');
+
+// mike.introduce();
+// mike.calcAge();
+
+// console.log(mike.__proto__); //Student
+// console.log(mike.__proto__.__proto__); //Person
+// console.log(mike instanceof Student); //true
+// console.log(mike instanceof Person); //true
+// console.log(mike instanceof Object); //true
+
+// //Lesson Coding challenge #3
+
+// class EVCl extends CarCl {
+//   #charge;
+//   constructor(make, speed, charge) {
+//     super(make, speed);
+//     this.#charge = charge;
+//   }
+//   chargeBattery(chargeTo) {
+//     this.#charge = chargeTo;
+//     return this;
+//   }
+//   accelerate() {
+//     this.speed += 20;
+//     this.#charge--;
+//     console.log(
+//       `${this.make} is going at ${this.speed} km/h, with a charge of ${
+//         this.#charge
+//       }`
+//     );
+//     return this;
+//   }
+// }
+
+// const tesla = new EVCl('Tesla', 120, 23);
+// tesla.accelerate().chargeBattery(90).accelerate();
+
+//Lesson 220: Inheritance between "Classes": ES6 Classes
+
+// class PersonCl {
+//   constructor(fullName, birthYear) {
+//     this.fullName = fullName;
+//     this.birthYear = birthYear;
+//   }
+//   calcAge() {
+//     console.log(2037 - this.birthYear);
+//   }
+// }
+
+// class StudentCl extends PersonCl {
+//   course;
+//   constructor(fullName, birthYear, course) {
+//     super(fullName, birthYear);
+//     this.course = course;
+//   }
+//   introduce() {
+//     console.log(`My name is ${this.fullName} and I study ${this.course}`);
+//   }
+// }
+
+// const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
+// martha.introduce();
+// martha.calcAge();
+
+//Lesson 221: Inheritance between "Classes": Object.create
+
 const PersonProto = {
   calcAge() {
     console.log(2037 - this.birthYear);
@@ -143,34 +276,70 @@ const PersonProto = {
 };
 
 const steven = Object.create(PersonProto);
-steven.init('Steven', 1991);
-console.log(steven);
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
 
-//Lesson 218: Coding challenge #2
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
 
-class CarCl {
-  constructor(make, speed) {
-    this.make = make;
-    this.speed = speed;
-  }
-  accelerate() {
-    console.log(`${this.make} gained speed: ${this.speed + 10}km/h`);
-  }
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'Computer Science');
+jay.introduce();
+jay.calcAge();
 
-  brake() {
-    console.log(`${this.make} lost speed: ${this.speed - 5}km/h`);
+//Lesson 222: Another class example
+
+class Account {
+  locale = navigator.language;
+  _movements = [];
+  _pin;
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this._pin = pin;
   }
-  get speedUS() {
-    return this.speed / 1.6;
+  getMovements() {
+    return this._movements;
   }
-  set speedUS(speed) {
-    this.speed = speed * 1.6;
+  deposit(val) {
+    this._movements.push(val);
+    return this;
+  }
+  withdraw(val) {
+    this.deposit(-val);
+    return this;
+  }
+  _approveLoan(val) {
+    return true;
+  }
+  requestLoan(val) {
+    if (this._approveLoan(val)) {
+      this.deposit(val);
+      console.log('Loan approved');
+      return this;
+    }
   }
 }
 
-const ford = new CarCl('Ford', 120);
-console.log(ford.speedUS);
-ford.speedUS = 50;
-console.log(ford);
+class CheckingAccount extends Account {
+  constructor(owner, currency, pin, interestRate) {
+    super(owner, currency, pin);
+    this.interestRate = interestRate;
+  }
+  _approveLoan(val) {
+    return val < this.interestRate * this.getBalance();
+  }
+}
 
-//Lesson 219: Inheritance between "Classes": Constructor Functions
+const acc1 = new CheckingAccount('Jonas', 'EUR', 1111, 0.1);
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+console.log(acc1);
+console.log(acc1.getMovements());
+
+//Lesson 223: Coding challenge #4
