3530344a983102972449b92bf3327170fa64a8f5
Petr Kroutil <116441828+Krouta1@users.noreply.github.com>
2024-02-29 15:29:17 +0100

    Lesson: 228

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,34 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+///Lesson 249: XMLHttpRequest
+const getCountryData = (country) => {
+    const request = new XMLHttpRequest();
+    request.open('GET',`https://restcountries.com/v3.1/name/${country}`);
+    request.send();
+    request.addEventListener('load',function(){
+        const [data] = JSON.parse(this.responseText)
+        const lang = data.languages[`${data.cca3.toLowerCase()}`] === undefined ? data.languages[`${data.cioc.toLowerCase()}`] : data.languages[`${data.cca3.toLowerCase()}`]
+        console.log(data.currencies)
+        const html= `
+            <article class="country">
+            <img class="country__img" src="${data.flags.png}" />
+            <div class="country__data">
+                <h3 class="country__name">${data.name.common}</h3>
+                <h4 class="country__region">${data.region}</h4>
+                <p class="country__row"><span>👫${(+data.population/1000000).toFixed(1)}</span> people</p>
+                <p class="country__row"><span>🗣️${lang}</span></p>
+                <p class="country__row"><span>💰${data.currencies['EUR'].name}</span></p>
+            </div>
+            </article>
+        `
+        countriesContainer.insertAdjacentHTML('beforeend',html)
+        countriesContainer.style.opacity = 1
+    })
+}
+
+getCountryData('germany')
+getCountryData('portugal')
+
+
+
